b4552d528340b18a404a564f683ec069f0afbb51
Janus Jerom <janussmartboy@gmail.com>
2022-10-27 23:12:42 +0530

    minor refactoring

--- C/binarysearch.c
+++ C/binarysearch.c
@@ -40,46 +40,4 @@ int main() {
   printf("\nElement is present at %d position of array", res);  
   return 0;  
 }  
-Output
-
-Binary Search Algorithm
-Program: Write a program to implement Binary search in C++.
-
-#include <iostream>  
-using namespace std;  
-int binarySearch(int a[], int beg, int end, int val)    
-{    
-    int mid;    
-    if(end >= beg)     
-    {  
-        mid = (beg + end)/2;    
-        if(a[mid] == val)    
-        {                 
-            return mid+1;    
-        }    
-        else if(a[mid] < val)     
-        {  
-            return binarySearch(a, mid+1, end, val);    
-        }    
-    else     
-        {  
-            return binarySearch(a, beg, mid-1, val);    
-        }         
-    }    
-    return -1;     
-}   
-int main() {  
-  int a[] = {10, 12, 24, 29, 39, 40, 51, 56, 70}; // given array  
-  int val = 51; // value to be searched  
-  int n = sizeof(a) / sizeof(a[0]); // size of array  
-  int res = binarySearch(a, 0, n-1, val); // Store result  
-  cout<<"The elements of the array are - ";  
-  for (int i = 0; i < n; i++)  
-  cout<<a[i]<<" ";    
-  cout<<"\nElement to be searched is - "<<val;   
-  if (res == -1)  
-  cout<<"\nElement is not present in the array";  
-  else  
-  cout<<"\nElement is present at "<<res<<" position of array";  
-  return 0;  
-}  
+ 
